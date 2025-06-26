4d9fa230bf8a9f9f1ec1ec64b251346739abbef9
moosewithwolf <moosewithwolf@gmail.com>
2025-06-26 12:34:30 -0400

    feat: axios instance setup

---
+++ src/api/request.js
@@ -0,0 +1,17 @@
+
+const mvRequests = {
+    fetchNowPlaying: "movie/now_playing",
+    fetchPopular: "movie/popular",
+    fetchTopRated: "movie/top_rated",
+    fetchTrending: "trending/movie/all/week",
+    fetchActionMovies: "/discover/movie?with_genres=28",
+    fetchComedyMovies: "/discover/movie?with_genres=35",
+    fetchHorrorMovies: "/discover/movie?with_genres=27",
+    fetchRomanceMovies: "/discover/movie?with_genres=10749",
+    fetchDocumentaries: "/discover/movie?with_genres=99",
+    fetchUpcoming: "movie/upcoming",
+}
+
+
+
+export default mvRequests;
